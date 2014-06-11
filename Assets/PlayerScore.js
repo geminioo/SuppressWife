2bd26476506b1d8f62f38230800d60131c441283
lineark <klineark@gmail.com>
2014-06-12 04:35:12 +0700

    Show Score in Leaderboard

---
+++ Assets/PlayerScore.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+var playerPointArray = [];
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI(){
+	var playerCount = PlayerPrefs.GetInt("playerCount");
+	Debug.Log(playerCount);
+	for(var i = 1 ; i<=playerCount ; i++){
+		var playerPoint = PlayerPrefs.GetInt(i.ToString());
+		// playerPointArray[i] = playerPoint;
+	// }
+	// playerPointArray.Sort();
+	// for(var j = 1 ; j<=playerPointArray.length ; j++){
+		GUI.Label(new Rect(Screen.width/4,Screen.height/4+(30*i) , 200f,30f) ,playerPoint+"");
+		// GUI.Label(new Rect(Screen.width/4,Screen.height/4+(30*j) , 200f,30f) ,playerPointArray[j]);
+	// }
+	}
+}
+
+// +(30*i)
--- Assets/Script/PlayScreen/Score.js
+++ Assets/Script/PlayScreen/Score.js
@@ -2,9 +2,11 @@
 
 static var point : int;
 var scoreStyle : GUISkin;
+var storePoint : int;
 
 function Start () {
 	point = 0;
+	storePoint = 0;
 	// scoreStyle = new GUIStyle();
 }
 
@@ -20,10 +22,17 @@ function OnGUI(){
     GUI.Label(Rect(Screen.width*3/5,Screen.height*1/50,Screen.width/10 ,Screen.height/8),point+"");
 
     if(Attacked.checkDie == 1){
+    	// Attacked.checkDie = 0;
     	// point = PlayerPrefs.GetInt("Player Score");
 		// PlayerPrefs.DeleteKey("Player Score");
-		var playerCount = PlayerPrefs.GetInt("playerCount");
-		PlayerPrefs.SetInt("Player Score", point);
-		PlayerPrefs.SetInt("playerCount", playerCount);
+		if(storePoint == 0){
+			var playerCount = PlayerPrefs.GetInt("playerCount");
+			playerCount++;
+			PlayerPrefs.SetInt(playerCount+"", point);
+			PlayerPrefs.SetInt("playerCount", playerCount);
+			Debug.Log("Stored!");
+			storePoint = 1;
+		}
+		// PlayerPrefs.DeleteAll();
 	}
 }
