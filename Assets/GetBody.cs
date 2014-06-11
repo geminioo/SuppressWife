3eca0a430386c314dea2c459268a52fe7927b2be
lukyth <k.sujautra@gmail.com>
2014-06-11 21:26:40 +0700

    add image & add enemy type

---
+++ Assets/GetBody.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetBody : MonoBehaviour {
+
+	private Sprite body;
+
+	void Start () {
+		this.gameObject.GetComponent<SpriteRenderer>().sprite = GameObject.Find("Enemy" + Random.Range(1,4)).gameObject.GetComponent<SpriteRenderer>().sprite;
+	}
+	
+	void Update () {
+	
+	}
+}
