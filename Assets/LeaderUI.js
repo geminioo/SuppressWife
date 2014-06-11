04fd7cd9409aa2dbf347cc8c9530754e330e90b2
lukyth <k.sujautra@gmail.com>
2014-06-12 03:42:08 +0700

    leaderboard

---
+++ Assets/LeaderUI.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+var leaderSkin : GUISkin;
+
+function Start () {
+	
+}
+
+function Update () {
+
+}
+
+function OnGUI(){
+	GUI.skin = leaderSkin;
+	if(GUI.Button(new Rect(Screen.width*8/9, Screen.height*1/20, Screen.width/14 ,Screen.height/10), "")){
+		Application.LoadLevel("FirstScreen");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Assets/Script/FirstScreen/HomeUI.js
+++ Assets/Script/FirstScreen/HomeUI.js
@@ -13,11 +13,11 @@ function Update () {
 
 function OnGUI(){
 	GUI.skin = homeSkin1;
-	if(GUI.Button(new Rect(Screen.width/4+20, Screen.height/2+40, 150 ,30), "")){
+	if(GUI.Button(new Rect(Screen.width*2/5, Screen.height*5/7, Screen.width/4 ,Screen.height/10), "")){
 		Application.LoadLevel("CameraScreen");
 	}
 	GUI.skin = homeSkin2;
-	if(GUI.Button(new Rect(Screen.width/4+20, Screen.height/2+75, 150 ,30), "")){
+	if(GUI.Button(new Rect(Screen.width*2/5, Screen.height*11/13, Screen.width/4 ,Screen.height/10), "")){
 		Application.LoadLevel("LeaderboardScreen");
 	}
 }
