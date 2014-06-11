c50b4d58b2b5b7dc0bb4605d165fc4d79301f579
lineark <klineark@gmail.com>
2014-06-12 03:32:55 +0700

    Store Score & Player Die

--- Assets/Script/PlayScreen/Attacked.js
+++ Assets/Script/PlayScreen/Attacked.js
@@ -1,16 +1,16 @@
 ﻿#pragma strict
 var playerHeart : GameObject;
-var count :int;
+static var checkDie :int;
+var windowRect : Rect;
 
 function Start () {
-	count = 0;
+	checkDie = 0;
+	windowRect = Rect(Screen.width/6+30, Screen.height/6+10, 480 ,300);
 }
 
 function Update () {
 	if(Input.GetKey(KeyCode.Space)){
-		// count++;
 		Score.point++;
-		Debug.Log(count);
 	}
 }
 
@@ -52,6 +52,20 @@ function OnTriggerEnter2D(col : Collider2D){
 		else if(GameObject.Find("coinSprite_1")){
 			playerHeart = GameObject.Find("coinSprite_1");
 			playerHeart.SendMessage("PlayerHeart");
+			checkDie = 1;
 		}
 	}
+}
+
+function OnGUI(){
+	if(checkDie == 1){
+		windowRect = GUI.Window (0, windowRect, DoMyWindow, "Pause");
+	}
+}
+
+function DoMyWindow (windowID : int) {
+	if (GUI.Button (Rect(Screen.width/7, Screen.height/6+10, 140 ,30), "Hello World"))
+		Application.LoadLevel("FirstScreen");
+	if (GUI.Button (Rect(Screen.width/7, Screen.height/6+50, 140 ,30), "Hello World"))
+		Application.LoadLevel("FirstScreen");
 }
--- Assets/Script/PlayScreen/Score.js
+++ Assets/Script/PlayScreen/Score.js
@@ -18,4 +18,12 @@ function OnGUI(){
 	// scoreStyle.alignment = TextAnchor.UpperRight;
 	GUI.skin = scoreStyle;
     GUI.Label(Rect(Screen.width/2+60f,Screen.height/4-100f,100,50),point+"");
+
+    if(Attacked.checkDie == 1){
+    	// point = PlayerPrefs.GetInt("Player Score");
+		// PlayerPrefs.DeleteKey("Player Score");
+		var playerCount = PlayerPrefs.GetInt("playerCount");
+		PlayerPrefs.SetInt("Player Score", point);
+		PlayerPrefs.SetInt("playerCount", playerCount);
+	}
 }
